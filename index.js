2220a2831e1fe3ab9f6564c6eb323e831e1a3a86
Abhishek Jain <abhishek.j0407@gmail.com>
2023-09-14 15:30:49 +0530

    Products section, cards design added

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 const menuBtn = document.getElementById('menuBtn');
 const openMenu = document.getElementById('open-menu')
 const closeMenu = document.getElementById('close-menu');
-const nav = document.getElementById('nav-items')
+const nav = document.getElementById('nav-items');
 
 function handleMenuClick() {
     nav.classList.toggle('hide');
     openMenu.classList.toggle('hide');
     closeMenu.classList.toggle('hide');
+}
+
+// Change active filter in Products
+const filters = document.getElementById('filters').children;
+function changeFilter(e) {
+    Array.from(filters).forEach(filter => {
+        filter.classList.remove('active');
+    });
+    e.target.classList.add('active');
 }
